16f652c193dc2d5b7b0f81b2167083e8948645f6
Chmielowski Dominik <w65529@student.wsiz.edu.pl>
2023-04-19 10:10:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Example from "./components/example";
 
 function App() {
   return (
     <div className="App">
+      <Example />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/example.js
@@ -0,0 +1,5 @@
+function Example() {
+  return <h1>IS THIS RENDERED</h1>;
+}
+
+export default Example;
