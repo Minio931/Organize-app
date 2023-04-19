c2fd89c2dd7baa514c04f249c7d3ba7075c43223
Chmielowski Dominik <w65529@student.wsiz.edu.pl>
2023-04-19 10:13:49 +0200

    Initial commit v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Example from "./components/example";
 
@@ -6,20 +5,6 @@ function App() {
   return (
     <div className="App">
       <Example />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
