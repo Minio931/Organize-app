b2c306e91589b5102bc530819189b3558004d337
Chmielowski Dominik <w65529@student.wsiz.edu.pl>
2023-05-04 13:02:27 +0200

    TodosView

--- organize-app-frontend/src/components/Dashboard/DashboardMain.js
+++ organize-app-frontend/src/components/Dashboard/DashboardMain.js
@@ -1,17 +1,25 @@
 import classes from "./DashboardMain.module.css";
 import HabitsView from "./HabitsView";
+import TodoView from "./TodoView";
 
 const DashboardMain = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const username = user.username;
   return (
-    <section>
-      <header className={classes.header}>
-        <h1 className={classes.welcome}>Welcome</h1>
-        <p className={classes["username-display"]}>{username}</p>
-      </header>
-      <HabitsView />
-    </section>
+    <>
+      <div className={classes["dashboard-body"]}>
+        <section>
+          <header className={classes.header}>
+            <h1 className={classes.welcome}>Welcome</h1>
+            <p className={classes["username-display"]}>{username}</p>
+          </header>
+          <HabitsView />
+        </section>
+        <section>
+          <TodoView />
+        </section>
+      </div>
+    </>
   );
 };
 
